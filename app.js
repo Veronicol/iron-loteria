478696efaae76840e598ddb69f0ca29ba233728e
Vero Celemin <veronica.celemin@gmail.com>
2018-12-22 13:03:31 +0100

    somos pobres

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const hbs = require('hbs');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const app = express();
+const axios = require('axios');
+const say = require('say');
 
 
 app.set('views', __dirname + '/views');
@@ -13,7 +15,28 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Routes go here!
+app.get('/', (req, res, next) => {
+  res.render('index');
+}) 
+
+
+app.post("/check-num", (req, res, next) => {
+  // const number = req.body.number;
+  const { number } = req.body;
+  axios.get(`http://api.elpais.com/ws/LoteriaNavidadPremiados?n=${number}`)
+    .then(response => {
+      const json = JSON.parse(response.data.split('=')[1])
+      // console.log('json:', json);
+      let num = json.numero;
+      let premio = json.premio;
+      say.speak(`${premio} eeeeeuuuuroooossss!`);
+      // res.send('prueba')
+      res.render('check', {num, premio});
+    })
+})
+
+
+
 
 app.listen(PORT, () => {
   console.info(`App listen at ${PORT} port`);
